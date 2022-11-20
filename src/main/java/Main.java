724649884c131cdb0ab8523dbc07e027b79bcfa8
ezhilkannan <ezhilkannan7@gmail.com>
2022-11-20 22:57:16 +0530

    Refactor to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 import static Utils.ReadFileUtil.readFile;
 
 
-public class Run {
+public class Main {
 
     public static void main(String[] args) throws Exception {
         String filePath = "/Users/ezhilkannan/Downloads/LedgerCoAssignment/test.txt";
