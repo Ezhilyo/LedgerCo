77f2f5632df973050b93cf146ab88b741442a5ab
ezhilkannan <ezhilkannan7@gmail.com>
2022-11-20 23:32:10 +0530

    Update build files

--- src/main/java/Geektrust.java
+++ src/main/java/Geektrust.java
@@ -3,10 +3,10 @@ import java.util.List;
 import static Utils.ReadFileUtil.readFile;
 
 
-public class Main {
+public class Geektrust {
 
     public static void main(String[] args) throws Exception {
-        String filePath = "/Users/ezhilkannan/Downloads/LedgerCoAssignment/test.txt";
+        String filePath = args[0];
         List<String> commands = readFile(filePath);
         new Service().runCommands(commands);
     }
